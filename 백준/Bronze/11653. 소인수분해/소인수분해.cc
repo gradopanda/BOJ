84cc6d4df95f37b6ab5c114a0b35ee7597c5f31a
gradopanda <145954245+gradopanda@users.noreply.github.com>
2023-11-15 17:04:04 +0900

    [Bronze I] Title: 소인수분해, Time: 32 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11653. 소인수분해/소인수분해.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int N;
+    cin>>N;
+
+    if(N==1){
+        return 0;
+    }
+    else{
+            for(int i=2;i<=N;i++)
+                while(N%i==0){
+                    cout<<i<<"\n";
+                        N/=i;
+                }
+                
+        }
+
+}
+
+
+

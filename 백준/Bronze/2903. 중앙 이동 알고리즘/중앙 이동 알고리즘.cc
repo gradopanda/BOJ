3795270a024060e184e6f17f5e705e8198d574d1
gradopanda <145954245+gradopanda@users.noreply.github.com>
2023-12-04 15:29:31 +0900

    [Bronze III] Title: 중앙 이동 알고리즘, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2903. 중앙 이동 알고리즘/중앙 이동 알고리즘.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int N;
+    int cnt=2;
+    cin>>N;
+    for(int i=1;i<=N;i++){
+            cnt=2*cnt-1;
+        
+    }
+    cout<<cnt*cnt;
+    
+    
+    return 0;
+    
+}
